0c2b2042257160c952fca0426f350d37c55a4a86
Gleb Mazovetskiy <glex.spb@gmail.com>
2017-01-22 14:24:55 +0000

    bundle gem thredded-markup-katex

---
+++ lib/thredded/markup/katex.rb
@@ -0,0 +1,9 @@
+require "thredded/markup/katex/version"
+
+module Thredded
+  module Markup
+    module Katex
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/thredded/markup/katex/version.rb
@@ -0,0 +1,7 @@
+module Thredded
+  module Markup
+    module Katex
+      VERSION = "0.1.0"
+    end
+  end
+end
