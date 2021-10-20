443d11aab019b14e8693460636354adadf3e05e4
Tim Diggins <tim@red56.uk>
2021-10-20 10:27:31 +0100

    accept rubocop suggestions

--- lib/generators/thredded/markdown_katex/install_generator.rb
+++ lib/generators/thredded/markdown_katex/install_generator.rb
@@ -7,21 +7,17 @@ module Thredded
       desc 'Installs Thredded Katex plugin.'
       public_task :install
 
-      def install # rubocop:disable Metrics/MethodLength
+      def install
         %w[application.scss _deps.scss].each do |scss_file|
           scss_path = File.join('app', 'assets', 'stylesheets', scss_file)
           next unless File.exist? scss_path
 
-          append_to_file scss_path, "\n" + '@import "_katex";' + "\n"
+          append_to_file scss_path, "\n@import \"_katex\";\n"
         end
         sass_path = 'app/assets/stylesheets/application.sass'
-        if File.exist? sass_path
-          append_to_file sass_path, "\n" + '@import "_katex"' + "\n"
-        end
+        append_to_file sass_path, "\n@import \"_katex\"\n" if File.exist? sass_path
         css_path = 'app/assets/stylesheets/application.css'
-        if File.exist? css_path # rubocop:disable Style/GuardClause
-          append_to_file css_path, "\n" + '//= require katex' + "\n"
-        end
+        append_to_file css_path, "\n//= require katex\n" if File.exist? css_path
       end
     end
   end
